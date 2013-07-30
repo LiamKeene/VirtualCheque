c24ec7594b02833d2582ce601a318d4a3d034299
Liam Keene <liam.keene@gmail.com>
2013-07-31 09:55:28 +1000

    Initial commit - Virtual Bank Cheque App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+VirtualCheque::Application.config.secret_key_base = '3b2a192baa2885c146efa3cf4ece62a3cfb522bc4495c8e117ccbe83e29dc82909e50445e1017950aa13981c6de5369622e61bacff2fa999c8eafe65d9d52f94'
