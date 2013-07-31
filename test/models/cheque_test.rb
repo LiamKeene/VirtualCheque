e00f4b229587ffded6f175ecc527ab87d4c3a29a
Liam Keene <liam.keene@gmail.com>
2013-08-01 00:02:45 +1000

    Adds method to Cheque model to format value in plain English
     modified:   app/models/cheque.rb - using linguistics gem convert a decimal
                  value into words
     modified:   test/models/cheque_test.rb - simple unittest for new method

--- test/models/cheque_test.rb
+++ test/models/cheque_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class ChequeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "value to words" do
+    @cheque = Cheque.new(payment_date: '2013-07-31', 
+      recipient: 'Test Person', value: 1303.48)
+    value_in_words = 'One Thousand Three Hundred and Three dollars \
+      and Forty-Eight cents'
+
+    assert_equal(value_in_words, @cheque.value_to_words, 
+      'Cheque should be able to output currency values in words.')
+  end
 end
